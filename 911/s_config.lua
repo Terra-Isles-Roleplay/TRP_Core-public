3b043a7a15547b79dfd2ba586377694b074ac2c0
Tecwizard <WhoviansUnite27@gmail.com>
2025-08-02 23:03:55 -0400

    Added server config for 911

---
+++ 911/s_config.lua
@@ -0,0 +1,9 @@
+ServerConfig = {}
+ServerConfig.devmode = false
+ServerConfig.cadapikey = ''
+--ServerConfig.cadapiurl = "https://cad.terra-isles.com/"
+--Comment out the above line and Uncomment the below line to switch to the Dev Version of the CAD
+--ServerConfig.cadapiurl = "https://cad.terra-isles.com/version-test/"
+
+-- Server ID to send with each 911 call
+ServerConfig.ServerID = 1
